6f21e1b85758978f8216bd19f1f5e5e52d887ea6
David Freire <dhfreire20@gmail.com>
2023-01-25 03:53:36 -0500

    Loading the data.
    The data is loaded into a PostgreSQL database using functions from another file.

---
+++ load_database.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import psycopg2 
+from dog_functions import *
+
+conn = psycopg2.connect(
+             host="localhost",
+             database="first_test_db",
+             port = "5432",
+             user="user",
+             password="password")
+
+print("Database connection started")
+
+cursor = conn.cursor()
+create_dog_table(conn, cursor)
+
+with open("dog_analysis.csv","r") as f:
+    populate_dog_table(conn,cursor,f)
+    
+
+closing(conn,cursor)
